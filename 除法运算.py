fade436c1641c7dfe77679264a52e7306b1a9da7
Ding Fuwei <dingfuwei813@163.com>
2023-12-14 18:29:07 +0800

    除法运算.py: "F:\1\除法运算.py"

--- 除法运算.py
+++ 除法运算.py
@@ -2,7 +2,7 @@ x = int(input("请输入运算的第一个数"))
 y = int(input("请输入运算的第二个数"))
 fr = int(input("请输入循环次数"))
 for i in range(fr):
-    if x > y:
+    if x >= y:
         count = x/y
         print(f'结果:{count}')
     else:
