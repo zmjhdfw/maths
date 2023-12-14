630b5fabc8ac493ef85890134804c623068b570e
Ding Fuwei <dingfuwei813@163.com>
2023-12-14 13:49:11 +0800

    减法运算.py: "F:\1\减法运算.py" 乘法运算.py: "F:\1\乘法运算.py"

--- 乘法运算.py
+++ 乘法运算.py
@@ -1,6 +1,6 @@
-x = int(input("请输入要运算的第一个数:"))
-y = int(input("请输入要运算的第二个数:"))
-fr = int(input("请输入要循环的次数:"))
+x = int(input("请输入运算的第一个数:"))
+y = int(input("请输入运算的第二个数:"))
+fr = int(input("请输入循环的次数:"))
 for i in range(fr):
     count = x*y
-    print(count)
+    print(f'结果:{count}')
--- 减法运算.py
+++ 减法运算.py
@@ -1,6 +1,6 @@
-a = int(input("请输入要运算的第一个数:"))
-b = int(input("请输入要运算的第二个数:"))
-fr = int(input("请输入要循环的次数:"))
+a = int(input("请输入运算的第一个数:"))
+b = int(input("请输入运算的第二个数:"))
+fr = int(input("请输入循环的次数:"))
 for i in range(fr):
     count = a-b
     print(f'结果:{count}')
