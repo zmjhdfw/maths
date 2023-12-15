e7a5d81ad87ff1a0a3b88684c61bfa53c1dbdd1d
Ding Fuwei <dingfuwei813@163.com>
2023-12-15 13:33:24 +0800

    阶乘运算.py: "f:\1\阶乘运算.py"

--- 阶乘运算.py
+++ 阶乘运算.py
@@ -1,7 +1,7 @@
-n = int(input("请输入运算的数:"))
+n = int(input("请输入运算的数字:"))
 if n >= 2:
     for i in range(n):
         i*=(i+1)
-    print(f'结果:{i}')
 else:
-    print(f'结果:1')
+    i = 1
+print(f'结果:{i}')
