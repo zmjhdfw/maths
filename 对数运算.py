7ca2e5f2fe5789825413b49c64e7f3ac417bc441
Ding Fuwei <dingfuwei813@163.com>
2023-12-16 20:30:24 +0800

    对数运算.py: "f:\1\对数运算.py"

--- 对数运算.py
+++ 对数运算.py
@@ -1,6 +1,9 @@
 import math
 x = int(input("请输入运算的数字:"))
 y = int(input("请输入底数:"))
-for i in range(1):
-    i = math.log(x,y)
-print(f'结果:{i}')
+if x > 0 and y > 0:
+    for i in range(1):
+        i = math.log(x,y)
+    print(f'结果:{i}')
+else:
+    print(f'非法操作，不予执行')
