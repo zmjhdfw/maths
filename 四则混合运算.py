29575fc477d707a574b5bcafb65eb5b3cd4ffc69
Ding Fuwei <dingfuwei813@163.com>
2023-12-18 09:08:50 +0800

    四则混合运算.py: "f:\1\四则混合运算.py"

--- 四则混合运算.py
+++ 四则混合运算.py
@@ -29,23 +29,23 @@ for i in range(5): #打印欢迎界面
 x = int(input("请输入运算的数字:")) #输入x变量的值
 y = int(input("请输入运算的数字:")) #输入y变量的值
 print(f'1.加法  2.减法  3.乘法  4.除法  5.退出') #打印选项
-for i in range(5):
+for i in range(5): #控制输入次数
     if i == 4:
         print(f'抱 歉 已 达 输 入 上 限 , 请 明 日 再 来 .')
         break
     else:
         n = int(input("请输入选项:")) #输入选项
         if n == 1:
-            a = addition() #定义选项a
+            a = addition() #定义选项1
         elif n == 2:
-            s = subtraction() #定义选项s
+            s = subtraction() #定义选项2
         elif n == 3:
-            m = multiplication() #定义选项m
+            m = multiplication() #定义选项3
         elif n == 4:
-            d = division() #定义选项d
+            d = division() #定义选项4
         elif n == 5:
-            print(f'期 待 下 次 与 你 相 见 !')
+            print(f'期 待 下 次 与 你 相 见 !') #定义选项5
             break
         else:
-            print(f'非法操作,不予执行')
+            print(f'非法操作,不予执行') #定义其他选项
             continue
