4f845ae2a7b1f246284ba11fa39e96c2f57e7ec9
丁富伟 <dingfuwei813@163.com>
2023-12-24 20:41:55 +0800

    '等差数列(条件已知).py': "d:\1\'等差数列(条件已知).py'"

--- 等差数列(条件已知).py
+++ 等差数列(条件已知).py
@@ -4,19 +4,18 @@ try:
     d = int(input("请输入等差数列公差:")) #要求用户输入公差
     n = int(input("请输入要求的项数:")) #要求用户输入项数
     if n !=0:
-        for i in range(1):
-            if x==0 and d==0:
-                y = x
-                print(f'这是首项为0,公差为0的常数数列,所以结果为{y}')
-            elif x!=0 and d==0:
-                y = x
-                print(f'这是首项不为0,但公差为0的常数数列,所以结果为{y}')
-            elif x==0 and d!=0:
-                y = (n-1)*d
-                print(f'这是首项为0,但公差不为0的常数数列,所以结果为{y}')
-            elif x!=0 and d!=0:
-                y = x + (n-1)*d
-                print(f'结果为{y}')
+        if x==0 and d==0:
+            y = x
+            print(f'这是首项为0,公差为0的常数数列,所以结果为{y}')
+        elif x!=0 and d==0:
+            y = x
+            print(f'这是首项不为0,但公差为0的常数数列,所以结果为{y}')
+        elif x==0 and d!=0:
+            y = (n-1)*d
+            print(f'这是首项为0,但公差不为0的常数数列,所以结果为{y}')
+        elif x!=0 and d!=0:
+            y = x + (n-1)*d
+            print(f'结果为{y}')
     else:
         print(f'项数为0无法计算,抱歉哈')
 except ValueError:
