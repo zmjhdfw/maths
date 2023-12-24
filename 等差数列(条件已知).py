ad8b9ad5e83922008a1a00ff143778f4e6e7b6a0
丁富伟 <dingfuwei813@163.com>
2023-12-24 20:51:31 +0800

    '等差数列(条件已知).py': "d:\1\'等差数列(条件已知).py'"

--- 等差数列(条件已知).py
+++ 等差数列(条件已知).py
@@ -3,20 +3,20 @@ try:
     x = int(input("请输入等差数列首项:")) #要求用户输入首项
     d = int(input("请输入等差数列公差:")) #要求用户输入公差
     n = int(input("请输入要求的项数:")) #要求用户输入项数
-    if n !=0:
-        if x==0 and d==0:
+    if n !=0: #如果n不为0的情况
+        if x==0 and d==0: #定义第一种情况
             y = x
             print(f'这是首项为0,公差为0的常数数列,所以结果为{y}')
-        elif x!=0 and d==0:
+        elif x!=0 and d==0: #定义第二种情况
             y = x
             print(f'这是首项不为0,但公差为0的常数数列,所以结果为{y}')
-        elif x==0 and d!=0:
+        elif x==0 and d!=0: #定义第三种情况
             y = (n-1)*d
             print(f'这是首项为0,但公差不为0的常数数列,所以结果为{y}')
-        elif x!=0 and d!=0:
+        elif x!=0 and d!=0: #定义第四种情况
             y = x + (n-1)*d
             print(f'结果为{y}')
-    else:
+    else: #如果n为0的情况
         print(f'项数为0无法计算,抱歉哈')
-except ValueError:
+except ValueError: #代码报错输出的情况
     print(f'公差为空值无法计算,抱歉哈')
