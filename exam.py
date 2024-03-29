a238fda7e5a8e98e365a22c3d9ef51cd25e09e65
丁富伟 <dingfuwei813@163.com>
2024-03-29 21:31:19 +0800

    等差数列.py: C:/数学公式/等差数列.py exam.py: C:/数学公式/exam.pys

---
+++ exam.py
@@ -0,0 +1,14 @@
+def test():
+    a = int(input(f'please give a number:'))
+    c = int(input(f'please give a number:'))
+    if a < 0:
+        for i in range(c):
+            a += 1
+        return a
+    elif a >= 0:
+        for i in range(c):
+            a -= 1
+        return a
+    else: 
+        print(f'非法输入')
+print(f'answer is {test()}')
---
+++ 等差数列.py
@@ -0,0 +1,26 @@
+x = int(input("请输入等差数列首项:"))
+
+
+try:
+    d = int(input("请输入等差数列公差:"))
+except ValueError:
+    print(f'抱歉无法获取公差')
+try:
+    d = int(input("请重新输入："))
+except ValueError:
+    for i in range(1):
+        print(f'因为没有公差，所以无法计算，抱歉哈')
+        break
+n = int(input("请输入要求的项数:"))
+if x==0 and d==0:
+    y = x
+    print(f'这是首项为0,公差为0的常数数列,所以结果为{y}')
+elif x!=0 and d==0:
+    y = xs
+    print(f'这是首项不为0,但公差为0的常数数列,所以结果为{y}')
+elif x==0 and d!=0:
+    y = (n-1)*d
+    print(f'这是首项为0,但公差不为0的常数数列,所以结果为{y}')
+elif x!=0 and d!=0:
+    y = x + (n-1)*d
+    print(f'结果为{y}')
