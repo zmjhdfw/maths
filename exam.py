08f0b2d3a54fa168cc95e4e2dd4d1f59fd3ea68d
zmjhdfw <dingfuwei813@163.com>
2025-08-20 23:35:42 +0800

    exam

--- exam.py
+++ exam.py
@@ -1,4 +1,25 @@
-from pathlib import Path
-path=Path()
-contents=path.read_text()
-print(contents)
+'''from getpass import getpass
+correct_username = 'admin'
+correct_password = '123456'
+username = input('请输入用户名')
+password = getpass('请输入密码')
+if username == correct_username and password == correct_password:
+    print(f'欢迎登陆')
+else:
+    print(f'非法操作')'''
+
+import tkinter as tk
+
+def create_child_windows():
+    master.withdraw()
+    child_windows = tk.Toplevel()
+    child_windows.title("子窗口")
+    child_windows.mainloop()
+
+master = tk.Tk()
+master.title("主窗口")
+
+btn_create_child = tk.Button(master,text="创建子窗口",command=create_child_windows)
+btn_create_child.pack()
+
+master.mainloop()
